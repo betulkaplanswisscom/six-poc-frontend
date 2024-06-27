69f5b4ae5c0d7c5560ab6889403479092dbc31e1
betulkaplan <betuel.kaplan@swisscom.com>
2024-06-27 13:48:59 +0200

    app config

--- src/app/session-in/session-in.component.ts
+++ src/app/session-in/session-in.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionService } from './session.service';
 
 @Component({
   selector: 'app-session-in',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './session-in.component.html',
-  styleUrl: './session-in.component.css'
+  styleUrls: ['./session-in.component.css'],
 })
-export class SessionInComponent {
+export class SessionInComponent implements OnInit {
+  lastAccessDate: string = '';
+  sessionMessage = 'This is session message';
 
+  constructor(private sessionService: SessionService, private router: Router) {}
+
+  ngOnInit() {
+    this.checkSession();
+  }
+
+  checkSession() {
+    this.sessionService.checkSession().subscribe(
+      (data: any) => {
+        if (!data.hasSession) {
+          this.router.navigate(['/login']);
+        } else {
+          this.lastAccessDate = data.lastAccessDate;
+        }
+      },
+      (error) => {
+        console.error('Error checking session:', error);
+        this.router.navigate(['/login']);
+      }
+    );
+  }
+
+  onAccept() {
+    this.sessionService.acceptSession().subscribe(
+      () => {
+        this.router.navigate(['/default']);
+      },
+      (error) => {
+        console.error('Error accepting session:', error);
+      }
+    );
+  }
+
+  onReturn() {
+    this.sessionService.returnSession().subscribe(
+      () => {
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        console.error('Error returning from session:', error);
+      }
+    );
+  }
 }
